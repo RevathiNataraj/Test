a91d23d501c3e00d21a5348d145026177a677dcb
RevathiNataraj <revathin274@gmail.com>
2022-08-16 14:06:05 +0530

    Project commited

---
+++ src/test/org/Test.java
@@ -0,0 +1,12 @@
+package test.org;
+
+public class Test {
+	private void test1() {
+		// TODO Auto-generated method stub
+System.out.println("test1");
+	}
+public static void main(String[] args) {
+	Test t = new Test();
+	t.test1();
+}
+}
