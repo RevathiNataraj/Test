9d1b8fd71ea8180d59a5562dc5445a16a7f74c51
RevathiNataraj <revathin274@gmail.com>
2022-08-16 15:53:46 +0530

    M Modified

--- src/test/org/Test.java
+++ src/test/org/Test.java
@@ -5,8 +5,19 @@ public class Test {
 		// TODO Auto-generated method stub
 System.out.println("test1");
 	}
+	private void test2() {
+		// TODO Auto-generated method stub
+System.out.println("test2");
+	}
+	private void test3() {
+		// TODO Auto-generated method stub
+System.out.println("test3");
+	}
 public static void main(String[] args) {
 	Test t = new Test();
 	t.test1();
+	t.test2();
+	t.test3();
 }
+
 }
